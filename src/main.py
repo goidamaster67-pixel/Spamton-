d054417360995fc2941a2f4998dcd45181f98874
goidamaster67-pixel <goidamaster67@gmail.com>
2026-04-09 23:31:45 +0700

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,3 +1,15 @@
+import os
+from threading import Thread
+from flask import Flask
+
+app = Flask('')
+@app.route('/')
+def home(): return "OK"
+
+def run():
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
+
+Thread(target=run).start()
 import telebot
 import random
 from g4f.client import Client
